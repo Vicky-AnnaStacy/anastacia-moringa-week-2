a23414591d42c624e514a9f0df4475577ec71bfa
Anastacia Ngendo Wainaina <ANWAINAINA@safaricom.co.ke>
2022-10-21 01:39:47 +0300

    modidfied variables

--- js/custom.js
+++ js/custom.js
@@ -2,17 +2,10 @@ const Male_Names = ["Kwasi","Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
 const Female_Names = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
 const Day_Of_The_Week = ["Sunday","Monday", "Tuesday", "Wednesday","Thursday","Friday","Saturday" ];
  
-var name;
-var date = null;
-var gender =  null;
-var username;
-var actualAkhanName;
-var year;
-var Month;
-var century;
-var day;
-var DayOfWeek;
-var akhanName = null;
+//variables for validation
+let validMonth =checkMonth();
+let validDay = checkDay();
+
  function Name(){
 let  year =document.getElementById("year").value;
 let month = number(document.getElementById("Month").value);
