c785f53086fc2cf717b985c2c8f7a2b849a74888
Anastacia Ngendo Wainaina <ANWAINAINA@safaricom.co.ke>
2022-10-20 23:43:45 +0300

    added variables

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+const Male_Names = ["Kwasi","Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame" ];
+const Female_Names = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama" ];
+const Day_Of_The_Week = ["Sunday","Monday", "Tuesday", "Wednesday","Thursday","Friday","Saturday" ];
+ 
+var name;
+var date = null;
+var gender =  null;
+var username;
+var actualAkhanName;
+var year;
+var Month;
+var century;
+var day;
+var DayOfWeek;
+var akhanName = null;
+
+
